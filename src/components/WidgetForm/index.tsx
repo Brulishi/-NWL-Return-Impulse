c162e7199192169884f5f5331b0789862da87ade
Brulishi <Bruli_pablo@hotmail.com>
2022-05-04 19:02:35 -0300

    [Feature Pablo - Parte 2 da aula completa]

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,6 +5,8 @@ import ideiaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { useState } from "react";
 import { FeedbackTypeStep } from "./steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./steps/FeedbackSuccessStep";
 
  export const feedbackTypes = {
     BUG: {
@@ -45,26 +47,32 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
 
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
-
+function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+}
 
     return (
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2ren)] md:w-auto" >
-        <header>
-        <span className="text-xl leading-6" >Deixe seu feedback</span>
-
-        <CloseButton/>
-        </header>
-
-
-        {!feedbackType ? (
-            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-            
-
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
         
+        {feedbackSent ? (
+            <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
+        ): (
+            <>
+             {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
                 ) :(
-                    <p>Hellow world</p>
+                    <FeedbackContentStep
+                     feedbackType={feedbackType}
+                     onFeedbackRestartRequested ={handleRestartFeedback}
+                    onFeedbackSent={() => setFeedbackSent(true)}
+                     />
                     )}
+            </>
+        )}
+       
 
 
         <footer className="text-xs text-neutral-400">
